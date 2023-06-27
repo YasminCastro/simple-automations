d47f15bf3615a01a1fb4a96d01ac9c13bc39ef2f
Yasmin Castro <yasminsdcastro@gmail.com>
2023-06-27 18:13:45 -0300

    feat: csv parse

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,36 @@
-import express from "express";
+import { Request, Response } from "express";
+import path from "path";
+import fs from "fs";
+import { parse } from "csv-parse";
 
+const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.use(express.json());
-
-app.get("/", (request, response) => {
-  return response.send("Hello world");
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "ok" });
 });
 
-app.listen(3000);
+app.get("/parse-expense", (req: Request, res: Response) => {
+  const dirPath = path.join(__dirname, "files");
+
+  const files = fs.readdirSync(dirPath);
+
+  const fileName = files[0];
+  const filePath = path.join(dirPath, fileName);
+
+  const file = fs.createReadStream(filePath, { encoding: "utf-8" });
+
+  console.log(file);
+
+  const parser = parse({ delimiter: "," }, (err, records) => {
+    console.log(records);
+  });
+
+  file.pipe(parser);
+  res.json({ message: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Running in http://localhost:${PORT}`);
+});
